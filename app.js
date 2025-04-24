1ed44c5dad0412a934efc4a38bcbb1bed13a5c38
Asadbek Tursunaliyev <madrahimoffasadbek@gmail.com>
2025-04-25 00:16:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+// class laptop {
+//   constructor(brand, ram, ssd) {
+//     this.brand = brand;
+//     this.ram = ram;
+//     this.ssd = ssd;
+//   }
+
+//   spec() {
+//     console.log(`Brand: ${this.brand}, RAM: ${this.ram}, SSD: ${this.ssd}`);
+//   }
+// }
+
+// const myLaptop = new laptop("ASUS", "16GB", "512GB");
+// myLaptop.spec();
+
+// 2
+// class Animal {
+//   constructor(type, sound) {
+//     this.type = type;
+//     this.sound = sound;
+//   }
+
+//   makeSound(){
+//     console.log(`Animal: ${this.type}, Sound: ${this.sound}`);
+//   }
+// }
+
+// const newAnimal = new Animal("It", "Vov")
+// newAnimal.makeSound()
+
+// 3
+// class Courses {
+//   constructor(name, duration, price) {
+//     this.name = name;
+//     this.duration = duration;
+//     this.price = price;
+//   }
+
+//   summary() {
+//     console.log(`name: ${this.name}, duration: ${this.duration}, price: ${this.price}`);
+//   }
+// }
+
+// const detailCourses = new Courses("reactjs", "3 oy", "$200");
+// detailCourses.summary();
+
+// 4
+// class Employee {
+//   constructor(name, salary, bonus = 0) {
+//     this.name = name;
+//     this.salary = salary;
+//     this.bonus = bonus;
+//   }
+
+//   getTotalIncome(){
+//     console.log(`salary: ${this.salary}`);
+//   }
+// }
+
+// const detailsEmployee = new Employee('John', '2000$', '10$')
+// detailsEmployee.getTotalIncome()
+
+// 5
+// class Pizza {
+//   constructor(size, ingredients) {
+//     this.size = size;
+//     this.ingredients = ingredients;
+//   }
+
+//   getInfo() {
+//     console.log(`${this.size} pizza: ${this.ingredients.join(", ")}`);
+//   }
+// }
+
+// const myPizza = new Pizza("Katta", ["sir", "kolbasa", "zaytun"]);
+// myPizza.getInfo();
+
+// 6
+// class Phone {
+//   constructor(brand, battery) {
+//     this.brand = brand;
+//     this.battery = battery;
+//   }
+
+//   charge(amount) {
+//     console.log(`Battery: ${this.battery}%`);
+//   }
+// }
+
+// const detailPhone = new Phone("Samsung", 80);
+// detailPhone.charge();
+
+// 7
+class Team {
+    constructor(teamName, members) {
+        
+    }
+}
